597afc807c98c4f9cf6e8b5b48832b59ace585fd
Raul Hernandez del Amo <43745318+RaulHernandezdelAmo@users.noreply.github.com>
2021-12-02 10:25:56 +0000

    Now a patient can have different services

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+
+/**
+ *
+ * @author Raul
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Clinic cl = new Clinic();
+        
+        cl.addPatient("Juan");
+        cl.addPatient("Pepe");
+        cl.addPatient("Marta");
+        cl.addPatient("Andrea");
+        
+        cl.addConsultation(new Date(), 200.5, "Head ache");
+        cl.addConsultation(new Date(), 10.89, "Cold");
+        cl.addMedicalExam(new Date(), 89.4, "Clinical Analyses");
+        cl.addMedicalExam(new Date(), 506.7, "Imaging");
+        cl.addMedicalExam(new Date(), 108.59, "Check-up");
+        
+        
+        cl.assignServicePatient(0, 4);
+        cl.assignServicePatient(1, 3);
+        cl.assignServicePatient(2, 2);
+        cl.assignServicePatient(3, 1);
+        cl.assignServicePatient(3, 0);
+    }
+    
+}
